3534c73fe14559d08de59e82a36617bb5e061872
Andrew Romasco <aromasco@umass.edu>
2020-08-31 10:54:57 -0400

    This is my initial Lab 1 commit.

---
+++ Lab1/lab1_AVVR.R
@@ -0,0 +1,43 @@
+---
+title: "lab1_AVVR"
+author: "Andrew Romasco"
+date: "8/24/2020"
+output: html_document
+---
+
+
+## Exercise 1
+
+x <- 3
+y <- 15
+x+y
+x-y
+x*y
+x/y
+
+
+
+## Exercise 2
+
+x <-2
+y <-3
+x^5 > y^4
+
+
+
+## Exercise 3
+
+vector <- c(22, 62, 148, 43, 129)
+sum <- sum(vector)
+sum
+freq.vector <- vector/sum
+freq.vector
+
+
+## Exercise 4
+
+DNA <- c('A', 'T', 'C', 'G')
+sort(DNA)
+DNA
+
+
